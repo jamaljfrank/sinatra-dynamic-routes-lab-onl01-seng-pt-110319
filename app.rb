c6d4861ca0cf012e3e074e3a7ca3233659a10779
Jamal Frank <jamaljfrank@gmail.com>
2020-02-17 00:15:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,6 +17,18 @@ class App < Sinatra::Base
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
   
-  get '/:operation/:number1/:number2' do 
-    params[:number1]
+  get '/:operation/:number1/:number2' do
+    num1 = params[:number1].to_i
+    num2 = params[:number2].to_i
+    case params[:operation]
+    when 'add'
+      (num1 + num2).to_s
+    when 'subtract'
+      (num1 - num2).to_s
+    when 'multiply'
+      (num1 * num2).to_s
+    when 'divide'
+      (num1 / num2).to_s
+    end
+  end
 end
